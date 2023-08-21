18e93b39e81fac9e144c242721e24779dfaf987f
Matthew <lanrecode23@gmail.com>
2023-08-21 14:55:28 +0100

    change

--- src/Components/Notfound.jsx
+++ src/Components/Notfound.jsx
@@ -6,17 +6,7 @@ function Notfound() {
   return (
     <div className="contain">
       <div>
-        <lottie-player
-          src="https://lottie.host/ff513abc-3863-446e-8d6a-c944a3a757df/IzkiYIeyKX.json"
-          background="#FFFFFF"
-          speed="1"
-          style={{ width: "300px", height: "300px" }}
-          loop
-         
-          autoplay
-          direction="1"
-          mode="normal"
-        ></lottie-player>
+        <img src="/images/not1.webp" alt="" />
       </div>
       <div>
       <h5>Go back to <Link to ="/">Home</Link></h5>
